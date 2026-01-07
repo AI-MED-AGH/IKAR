ffcef260eeea8230d9c774bbca7a5d9c7664adb5
KamilOpyrchal <kamilop13@interia.eu>
2026-01-07 18:54:55 +0100

    Added example of AI request

---
+++ mobile_app/client.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import datetime
+
+URL = "https://ikar.onrender.com/api/device/event"
+
+
+timestamp = datetime.now().isoformat()
+
+data = {
+    "device_id": "1",
+    "timestamp": timestamp,
+    "data": {
+        "event": "fall detected",
+        "confidence": 0.99
+    }
+}
+
+try:
+    response = requests.post(URL, json=data)
+    response.raise_for_status()
+    print("Status:", response.status_code)
+    print("Server response:", response.json())
+
+except Exception as e:
+    print(f"Wystąpił błąd: {e}")
