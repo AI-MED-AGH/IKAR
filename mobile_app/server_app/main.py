4db3526521e134d0cf11a60218467c8249717cfa
KamilOpyrchal <kamilop13@interia.eu>
2026-01-11 21:14:50 +0100

    Added basic HTML form handling

--- mobile_app/server_app/main.py
+++ mobile_app/server_app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, Request, HTTPException, status, Cookie, Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 from pydantic import BaseModel
@@ -7,15 +7,21 @@ from typing import Annotated
 from jose import JWTError, jwt
 import os
 from dotenv import load_dotenv 
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+
+templates = Jinja2Templates(directory="templates")
 
 load_dotenv() # loading env variables
 
 app = FastAPI()
 
+
+
 #Configuration
-SECRET_KEY = os.getenv("SECRET_KEY") #string on which base we generate our token
-ALGORITHM = os.getenv("ALGORITHM") # algorithm which we are using to generate our token
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")) # time that our token is valid
+SECRET_KEY = os.getenv("SECRET_KEY","Whatever") #string on which base we generate our token
+ALGORITHM = os.getenv("ALGORITHM", "HS256") # algorithm which we are using to generate our token
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES","30")) # time that our token is valid
 
 #Hash Configuration (using bcrypt)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # hashing and verifying password
@@ -46,8 +52,27 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM) # generating our token
     return encoded_jwt 
 
+def check_access_token(access_token : Annotated[str, Cookie()] = None):
+    if access_token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="invalid token"
+        )
+    access_token = access_token.replace("Bearer ", "")
+    print (access_token)
+
+    try:
+        tokenData = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = tokenData["sub"]
+        return username
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="invalid token"
+        )
+
 @app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()): # in form_data we have login and password that client type in
+async def login( response: Response ,form_data: OAuth2PasswordRequestForm = Depends()): # in form_data we have login and password that client type in
     user = users.get(form_data.username) # here we have data of our user, if exists
 
 
@@ -58,19 +83,17 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()): # in form_dat
            headers={"WWW_Authenticate": "Bearer"},
         )
     access_token = create_access_token(data={"sub": user["username"]}) # if everything is correct we are creating token of user
-    return {"access_token": access_token, "token_type": "bearer"} # and give it to oauth2_scheme
+    response = RedirectResponse(url="/users/me", status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie(
+        key="access_token", 
+        value=f"Bearer {access_token}", 
+        httponly=True  # Ważne dla bezpieczeństwa
+    )
+    return response 
 
 @app.get("/users/me")
-async def read_users_me(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) # we decode our token
-
-        username: str = payload.get("sub") # now we have dict with sub and exp so we can get our username
-
-        if username is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
+async def read_users_me(token: str = Depends(check_access_token)):
+    username : str = token
     
     return {"user": username, "message": "NICE!"}
 
@@ -98,3 +121,10 @@ def receive_device_event(event: DeviceEvent):
         "status": "ok",
         "message": "Event received successfully"
     }
+
+@app.get("/")
+def main(request: Request):
+    return templates.TemplateResponse(
+        request= request,
+        name="index.html"
+    )
